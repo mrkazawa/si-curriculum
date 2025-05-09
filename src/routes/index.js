ecf0c366d2b0440622577cd3e39e34e94742f895
mrkazawa <yustus.oktian@gmail.com>
2025-05-09 09:18:29 +0700

    change the appearance of home to make it a dashboard

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,13 @@ const bkCplMkMappingRoutes = require("./bk/bk-cpl-mk-mapping-routes");
 const mkBkMappingRoutes = require("./mk/mk-bk-mapping-routes");
 const mkCplMappingRoutes = require("./mk/mk-cpl-mapping-routes");
 
+// Import models for dashboard
+const PlModel = require("../models/pl-model");
+const CplModel = require("../models/cpl-model");
+const CpmkModel = require("../models/cpmk-model");
+const BkModel = require("../models/bk-model");
+const MkModel = require("../models/mk-model");
+
 const setRoutes = (app) => {
   // Middleware to make path available to all views
   app.use((req, res, next) => {
@@ -18,12 +25,65 @@ const setRoutes = (app) => {
     next();
   });
 
-  // Home route
+  // Home route with dashboard data
   app.get("/", (req, res) => {
-    res.render("index", {
-      title: "Program Accreditation System",
-      message: "Welcome to Curriculum App",
-    });
+    // Use Promise.all to fetch all counts in parallel
+    Promise.all([
+      new Promise((resolve, reject) => {
+        PlModel.getAll((err, results) => {
+          if (err) reject(err);
+          else resolve(results ? results.length : 0);
+        });
+      }),
+      new Promise((resolve, reject) => {
+        CplModel.getAll((err, results) => {
+          if (err) reject(err);
+          else resolve(results ? results.length : 0);
+        });
+      }),
+      new Promise((resolve, reject) => {
+        CpmkModel.getAll((err, results) => {
+          if (err) reject(err);
+          else resolve(results ? results.length : 0);
+        });
+      }),
+      new Promise((resolve, reject) => {
+        BkModel.getAll((err, results) => {
+          if (err) reject(err);
+          else resolve(results ? results.length : 0);
+        });
+      }),
+      new Promise((resolve, reject) => {
+        MkModel.getAll((err, results) => {
+          if (err) reject(err);
+          else resolve(results ? results.length : 0);
+        });
+      }),
+    ])
+      .then(([plCount, cplCount, cpmkCount, bkCount, mkCount]) => {
+        res.render("index", {
+          title: "Program Accreditation System",
+          message: "Welcome to Curriculum App",
+          plCount,
+          cplCount,
+          cpmkCount,
+          bkCount,
+          mkCount,
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching dashboard data:", error);
+        res.render("index", {
+          title: "Program Accreditation System",
+          message: "Welcome to Curriculum App",
+          plCount: 0,
+          cplCount: 0,
+          cpmkCount: 0,
+          bkCount: 0,
+          mkCount: 0,
+          error: "Failed to load dashboard data",
+        });
+      });
   });
 
   // Use the new PL routes with updated path structure
